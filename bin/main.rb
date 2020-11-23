1eb8306584085714c0acb147fb5f23672ceabfe0
Naqhid <mnaqhid@gmail.com>
2020-11-23 14:17:44 +0530

    create a class monitor for TicTacToe game

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-puts "hello world"
+require_relative("../lib/game")
+require_relative("../lib/player")
+require_relative("../lib/board")
+$b = Board.new
+class Monitor
+  def initialize
+  puts "Welcome to Tic Tac Toe game \n\n "
+  end
+  end
+
+
+
+
