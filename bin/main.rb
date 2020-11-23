b54a53d1e05772f6b90ac82817f14aa5503413ac
Naqhid <mnaqhid@gmail.com>
2020-11-23 14:20:37 +0530

    Define method validate_name

--- bin/main.rb
+++ bin/main.rb
@@ -7,7 +7,15 @@ class Monitor
   def initialize
   puts "Welcome to Tic Tac Toe game \n\n "
   end
+
+  until Game.validate_name(player_2_name,'o') 
+    puts 'Name is too short,try again'
+    player_2_name = gets.chomp.strip.capitalize
   end
+
+  end
+
+
 
 
 
