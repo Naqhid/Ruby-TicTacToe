9c0ac2fc969057763ea2f2999639147a0948344c
jamestucker1988 <xat2212@gmail.com>
2020-11-23 22:41:25 +0530

    removed unnecessary files

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,1 @@
-# require_relative("./board")
-# $store = [ ]
-# class Player
-
-#   attr_reader :name, :sign
-
-#   def self.store1
-#     $store
-#   end
-#     def initialize(name, sign)
-#       @name = name
-#       @sign = sign
-#     $store << @name
-#     end
-#   end 
+# 
