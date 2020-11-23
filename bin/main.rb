d705a9d3eb168115bb4da3ab64993fe83a91b3b4
jamestucker1988 <xat2212@gmail.com>
2020-11-23 14:54:16 +0530

    call function win update and declare result of match

--- bin/main.rb
+++ bin/main.rb
@@ -38,7 +38,43 @@ class Monitor
  
   end
 
+  m = Monitor.new
 
+  m.display_board
+  m.player_input 
+  c=0
+  # p = player.new
+  while c < 9
+    
+  if c % 2 == 0 
+       puts "player1 make ur move and choose which square u like "
+       $b.win_update
+       $d = m.player_move
+       
+      #  $b.display_board
+       until  $b.update_board($d,'x') 
+        $b.update_board($d,'x') 
+        $b.win_update
+         $d = m.player_move 
+       
+       end
+       $b.display_board
+  else
+    puts "player2 make ur move and choose which square u like"
+    $b.win_update
+    $d = m.player_move
+    
+    # $b.display_board
+    until  $b.update_board($d,'o')  
+      $b.update_board($d,'o')  
+      $b.win_update
+     $d = m.player_move
+    end
+    $b.display_board
+    
+  end
+  c += 1
+  end
 
 
 
