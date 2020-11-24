27402a8ce20756782419bfdc4c424f002352e6a6
Naqhid <mnaqhid@gmail.com>
2020-11-24 14:24:29 +0530

    update board.rb

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+
+require_relative("../lib/player")
+class Board
+  @@view_board = [0,1,2,3,4,5,6,7,8] 
+  @@combinations = [ [0,1,2], [3,0,6],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[0,4,8],[6,4,2] ]
+      
+         def initialize
+        
+             @true_board = [10, 10, 10, 10, 10, 10, 10, 10, 10] 
+           
+         end
+  def display_board 
+    puts @@view_board[0].to_s + '|' + @@view_board[1].to_s + '|' + @@view_board[2].to_s
+    puts '-----'
+    puts @@view_board[3].to_s + '|' + @@view_board[4].to_s + '|' + @@view_board[5].to_s
+    puts '-----'
+    puts @@view_board[6].to_s + '|' + @@view_board[7].to_s + '|' + @@view_board[8].to_s
+  end
+  end
