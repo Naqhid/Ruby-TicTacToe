ae3ceeb28eef3ba3a65f2e8d2eeea6bb2b41b6fb
jamestucker1988 <xat2212@gmail.com>
2020-11-24 14:02:47 +0530

    define method win_update to check for player win

--- lib/game.rb
+++ lib/game.rb
@@ -19,4 +19,23 @@ class  Game
         puts  "invalid move try again!"
         return false
       end
+      def win_update
+        puts "win_update"
+         @@combinations.each  do |e| 
+           sum = @true_board[e[0]] + @true_board[e[1]] + @true_board[e[2]]
+           if  sum == 0
+             
+             puts " yes#{Player.store1[0]} won"
+             exit 1
+           elsif sum == 3
+              puts " winner is #{Player.store1[1]} won"
+             puts " no"
+             exit 1
+     
+           end
+           end
+           return false
+         
+         end
+         
   end
