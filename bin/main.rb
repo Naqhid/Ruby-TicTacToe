70cc17d270b69cea09e6682c2257b16c07ed199b
Naqhid <mnaqhid@gmail.com>
2020-11-30 13:58:23 +0530

    fix permission error

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-require_relative '../lib/player.rb'
-require_relative '../lib/game.rb'
+require_relative ('../lib/player.rb')
+require_relative ('../lib/game.rb')
 
 puts "Welcome to Tic Tac Toe game \n\n "
 
