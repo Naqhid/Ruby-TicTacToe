64d22162daa685198f1911601c633df6e66c25f1
Naqhid <mnaqhid@gmail.com>
2020-12-08 23:12:26 +0530

    rectify line length errors

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,33 +1,34 @@
 require './lib/board.rb'
 
 RSpec.describe 'testing the board' do
+  let(:board) {Board.new}
   it 'should show us an empty board' do
-    expect(Board.new.show_board).to eq("+------------------------+\n| 1 - ❔  2 - ❔  3 - ❔ | \n| 4 - ❔  5 - ❔  6 - ❔ | \n| 7 - ❔  8 - ❔  9 - ❔ | \n+------------------------+")
+    expect(board.squares).to eq([{ '1': nil }, { '2': nil }, { '3': nil }],[{ '4': nil }, { '5': nil }, { '6': nil }],[{ '7': nil }, { '8': nil }, { '9': nil }])
   end
 
   it 'should show us an empty board' do
-    expect(Board.new.show_board).not_to eq("+------------------------+\n| 1 - p  2 - ❔  3 - ❔ | \n| 4 - ❔  5 - ❔  6 - ❔ | \n| 7 - ❔  8 - ❔  9 - ❔ | \n+------------------------+")
+    expect(board.squares).not_to eq([{ '1': nil }, { '2': nil }, { '3': nil }],[{ '4': nil }, { '5': nil }, { '6': nil }],[{ '7': nil }, { '8': nil }, { '9': nil }])
   end
 
   context 'update board methods' do
     it 'should return a board with an input in 5th position' do
-      expect(Board.new.update_board(5, 'x')).not_to eq("+------------------------+\n| 1 - ❔  2 - x  3 - ❔ | \n| 4 - ❔  5 - ❌  6 - ❔ | \n| 7 - ❔  8 - ❔  9 - ❔ | \n+------------------------+")
+      expect(board.update_board(5, 'x')).not_to eq([{ '1': nil }, { '2': nil }, { '3': nil }],[{ '4': nil }, { '5': ❌ }, { '6': nil }],[{ '7': nil }, { '8': nil }, { '9': nil }])
     end
 
     it 'should return a board with an input in 5th position' do
-      expect(Board.new.update_board(5, 'x')).to eq("+------------------------+\n| 1 - ❔  2 - ❔  3 - ❔ | \n| 4 - ❔  5 - ❌  6 - ❔ | \n| 7 - ❔  8 - ❔  9 - ❔ | \n+------------------------+")
+      expect(board.update_board(5, 'x')).to eq([{ '1': nil }, { '2': nil }, { '3': nil }],[{ '4': nil }, { '5': ❌ }, { '6': nil }],[{ '7': nil }, { '8': nil }, { '9': nil }])
     end
 
     it 'should return a board with an "o" in 5h position if 9th position is x' do
-      board = Board.new
+      
       board.update_board(9, 'x')
-      expect(board.update_board(5, 'o')).to eq("+------------------------+\n| 1 - ❔  2 - ❔  3 - ❔ | \n| 4 - ❔  5 - ⭕  6 - ❔ | \n| 7 - ❔  8 - ❔  9 - ❌ | \n+------------------------+")
+      expect(board.update_board(5, 'o')).to eq([{ '1': nil }, { '2': nil }, { '3': nil }],[{ '4': nil }, { '5': ⭕ }, { '6': nil }],[{ '7': nil }, { '8': nil }, { '9': ❌ }])
     end
 
     it 'should return a board with an "o" in 5h position if 9th position is x' do
-      board = Board.new
+    
       board.update_board(9, 'x')
-      expect(board.update_board(5, 'o')).not_to eq("+------------------------+\n| 1 - ❔  2 - ❔  3 - ❔ | \n| 4 - ❔  5 - X  6 - ❔ | \n| 7 - ❔  8 - ❔  9 - ❌ | \n+------------------------+")
+      expect(board.update_board(5, 'o')).not_to eq([{ '1': nil }, { '2': nil }, { '3': nil }],[{ '4': nil }, { '5': nil }, { '6': nil }],[{ '7': nil }, { '8': nil }, { '9': ❌ }])
     end
   end
 end
